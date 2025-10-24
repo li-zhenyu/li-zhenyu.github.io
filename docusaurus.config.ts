c3ed51fddfb5b91adc4b8e91428293683dfd129c
anonymous <anonymous@moc.com>
2025-10-24 16:26:55 +0800

    Fixed the config as warned

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,6 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
+  trailingSlash: false,
   deploymentBranch: 'master',
   title: '李振宇的主页',
   tagline: '',
