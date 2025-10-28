2f7a80343543f7cb64ad66e85ac70c1acc2a8b67
li-zhenyu <53037841+li-zhenyu@users.noreply.github.com>
2025-10-28 22:18:51 +0800

    更新网站配置，将生产网址从 GitHub Pages 更改为 Pages.dev

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://li-zhenyu.github.io',
+  url: 'https://li-zhenyu.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
