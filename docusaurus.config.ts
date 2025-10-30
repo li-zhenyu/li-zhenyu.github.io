ddb44287e83bf0807f89ad3d11ab4d5781b705cc
li-zhenyu <53037841+li-zhenyu@users.noreply.github.com>
2025-10-30 21:05:37 +0800

    修复RSS链接路径，确保正确指向博客的RSS源

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -100,7 +100,7 @@ const config: Config = {
           label: '文档',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {to: '/blog/rss.xml', label: 'RSS', position: 'right'},
+        {to: 'pathname:///blog/rss.xml', label: 'RSS', position: 'right'},//Fuck you Docusaurus!
         {to: '/about', label: '关于', position: 'right'},
         {
           href: 'https://github.com/li-zhenyu/',
