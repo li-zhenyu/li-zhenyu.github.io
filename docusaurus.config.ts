a116666c457efea9dc7186d8f65c7cbafbe9bf22
li-zhenyu <53037841+li-zhenyu@users.noreply.github.com>
2025-11-03 14:00:32 +0800

    fix rss page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,8 @@ const config: Config = {
         blog: {
           showReadingTime: true,
           showLastUpdateTime: true,
+          blogSidebarTitle: '全部文章',//默认为“Recent Posts”
+          blogSidebarCount: 'ALL',
           feedOptions: {
             type: 'all',
             copyright: `版权所有 © ${new Date().getFullYear()} 李振宇`,
@@ -144,15 +146,15 @@ const config: Config = {
         //     },
         //   ],
         // },
-        {
-          title: '友链',
-          items: [
-            {
-              label: 'FrankSean的树洞',
-              href: 'https://bili233.top/',
-            },
-          ],
-        }
+        // {
+        //   title: '友链',
+        //   items: [
+        //     {
+        //       label: 'FrankSean的树洞',
+        //       href: 'https://bili233.top/',
+        //     },
+        //   ],
+        // }
       ],
       copyright: `版权所有 © ${new Date().getFullYear()} 李振宇　由 Docusaurus 驱动`,
     },
--- src/pages/rss.md
+++ src/pages/rss.md
@@ -6,7 +6,10 @@ title: RSS
 
 读者可以选择以下链接其一，使用新闻阅读器订阅本站：
 
-- [RSS](/rss.xml)
-- [Atom](/atom.xml)
+- [RSS](pathname:///blog/rss.xml)
+- [Atom](pathname:///blog/atom.xml)
+- [JSON](pathname:///blog/feed.json)
+
+<!--Fuck you again, Docusaurus-->
 
 虽然我自己也不怎么使用新闻阅读器了。但是Feed这种东西，用起来比较方便，体验也比较有格调，属于“可以不用但不能没有”的站点要素吧😊。
