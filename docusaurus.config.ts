176fdb084fc6aad2379175a04668511e2ee58daa
li-zhenyu <53037841+li-zhenyu@users.noreply.github.com>
2025-11-05 22:50:25 +0800

    add links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,15 +1,15 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   trailingSlash: false,
-  deploymentBranch: 'master',
-  title: '李振宇的主页',
-  tagline: '🫡 欢迎！',
-  favicon: '/img/avatar.ico',
+  deploymentBranch: "master",
+  title: "李振宇的主页",
+  tagline: "🫡 欢迎！",
+  favicon: "/img/avatar.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -17,48 +17,48 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://lizhenyu.top',
+  url: "https://lizhenyu.top",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'li-zhenyu', // Usually your GitHub org/user name.
-  projectName: 'li-zhenyu.github.io', // Usually your repo name.
+  organizationName: "li-zhenyu", // Usually your GitHub org/user name.
+  projectName: "li-zhenyu.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans'],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/li-zhenyu/li-zhenyu.github.io/tree/source/',
+            "https://github.com/li-zhenyu/li-zhenyu.github.io/tree/source/",
         },
         blog: {
           showReadingTime: true,
           showLastUpdateTime: true,
-          blogSidebarTitle: '全部文章',//默认为“Recent Posts”
-          blogSidebarCount: 'ALL',
+          blogSidebarTitle: "全部文章", //默认为“Recent Posts”
+          blogSidebarCount: "ALL",
           feedOptions: {
-            type: 'all',
+            type: "all",
             copyright: `著作权所有 © ${new Date().getFullYear()} 李振宇`,
             createFeedItems: async (params) => {
-              const {blogPosts, defaultCreateFeedItems, ...rest} = params;
+              const { blogPosts, defaultCreateFeedItems, ...rest } = params;
               return defaultCreateFeedItems({
                 // keep only the 50 most recent blog posts in the feed
                 blogPosts: blogPosts.filter((item, index) => index < 50),
@@ -69,14 +69,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-           'https://github.com/li-zhenyu/li-zhenyu.github.io/tree/source/',
+            "https://github.com/li-zhenyu/li-zhenyu.github.io/tree/source/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -89,43 +89,48 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: '首页',
+      title: "首页",
       logo: {
-        alt: '网站 Logo',
-        src: 'img/logo.svg',
+        alt: "网站 Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: '文档',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "文档",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {to: '/rss', label: 'RSS', position: 'right'},//Fuck you Docusaurus!
-        {to: '/about', label: '关于', position: 'right'},
+        { to: "/blog", label: "Blog", position: "left" },
+        { to: "/rss", label: "RSS", position: "right" }, //Fuck you Docusaurus!
+        { to: "/links", label: "友链", position: "left" },
+        { to: "/about", label: "关于", position: "right" },
         {
-          href: 'https://github.com/li-zhenyu/',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/li-zhenyu/",
+          label: "GitHub",
+          position: "right",
         },
         {
           href: "https://github.com/li-zhenyu/li-zhenyu.github.io/discussions/1",
-          label: '留言板',
-          position: 'right',
+          label: "留言板",
+          position: "right",
         },
-        {href: 'mailto:lizhenyu66666666@outlook.com', label: '邮箱', position: 'right'},
+        {
+          href: "mailto:lizhenyu66666666@outlook.com",
+          label: "邮箱",
+          position: "right",
+        },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: '文档',
+          title: "文档",
           items: [
             {
-              label: '奶龙都能看懂的民法总则手册',
-              to: '/docs/civil',
+              label: "奶龙都能看懂的民法总则手册",
+              to: "/docs/civil",
             },
           ],
         },
@@ -168,7 +173,7 @@ const config: Config = {
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 export default config;
---
+++ src/pages/links.md
@@ -0,0 +1,16 @@
+---
+title: 友链
+---
+
+# 友链
+
+[写信](mailto:lizhenyu66666666@outlook.com)给我以互换友链。
+
+示例：
+
+```yaml
+intro: "your_website_introduction"
+link: "http://to-your-website.domain"
+```
+
+或者简单文字介绍一下，亦可。
