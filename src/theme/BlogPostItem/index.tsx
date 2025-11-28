a037f4ea51f89b463c4fe89a9f5b14224671598d
li-zhenyu <53037841+li-zhenyu@users.noreply.github.com>
2025-11-28 20:33:51 +0800

    TEST add cusdis on blog page

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -3,6 +3,7 @@ import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type {WrapperProps} from '@docusaurus/types';
 import {useBlogPost} from '@docusaurus/plugin-content-blog/client'
+import { ReactCusdis } from 'react-cusdis';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
@@ -10,10 +11,22 @@ export default function BlogPostItemWrapper(props: Props): ReactNode {
   const {metadata} = useBlogPost();
   const {title, permalink} = metadata;
   const inside_title = title.replaceAll("〈", "《").replaceAll("〉", "》").replaceAll("《", "〈").replaceAll("》", "〉");//按照权威机构，内部第三层次的书名号，回到《》。
+
   return (
     <>
       <BlogPostItem {...props} />
-      <a href={`mailto:lizhenyu66666666@outlook.com?subject=关于博客文章《${inside_title}》的邮件`}>（暂代评论区）就《{inside_title}》给作者写邮件</a>
+    <div>
+      <ReactCusdis
+        attrs={{
+          host: 'https://cusdis.com',
+          appId: '3ae124ed-37d0-46c0-84f8-e1feec7bee8a',
+          pageId: permalink,
+          pageTitle: title,
+          pageUrl: "https://lizhenyu.top" + permalink
+        }}
+      />
+
+    </div>
     </>
   );
 }
